0e1f8e4760e5c1f799929856c88378a7094bb88e
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-02-14 19:11:06 +0900

    feat:css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import "./css/bucket.css";
+import "./css/reset.css";
 import "./css/common.scss";
-import book from "./data/Book";
 
+import book from "./data/Book";
+// fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCirclePlus,
+  faSquareMinus,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div className="App">
       <body className="bg center">
-        <h1 className="title">2023 독서 계획</h1>
-        <p className="msg">2023년에 읽을 도서</p>
-        <div className="row flex-row wrap">
-          {book.map((a, i) => {
-            return (
-              <div className="bucket img2 center ">
-                <h4>{book[i].title}</h4>
-              </div>
-            );
-          })}
+        <div className="container">
+          <h1 className="title">2023 독서 계획</h1>
+          <p className="msg">2023년에 읽을 도서</p>
+          <div className="icon">
+            {" "}
+            <FontAwesomeIcon
+              icon={faCirclePlus}
+              className="circlePlus cursor"
+            />
+            <FontAwesomeIcon
+              icon={faSquareMinus}
+              className="squareMinus cursor"
+            />
+          </div>
+          <div className="category">
+            <div className="menu">
+              <ul>
+                <li>전체보기</li>
+                <li>즐겨찾기</li>
+              </ul>
+            </div>
+            <div className="search">
+              {" "}
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="faMagnifyingGlass cursor"
+              />
+            </div>
+          </div>
+          <div className="row flex-row wrap">
+            {book.map((a, i) => {
+              return (
+                <div className="bucket img2 center ">
+                  <h4>{book[i].title}</h4>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </body>
     </div>
